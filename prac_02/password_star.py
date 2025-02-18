ebb09e8fada1fa139ba5416f63e98c2e9f09a55b
hein <heinhtetaung1@my.jcu.edu.au>
2025-02-18 21:04:09 +0800

    Creating a function to handle validating the password and used the constant to compare the length of the word

---
+++ prac_02/password_star.py
@@ -0,0 +1,13 @@
+MINIMUM_LENGTH = 18
+def main():
+    """Printing the  asterisks as long as the word"""
+    password = get_valid_password()
+    print(len(password)*"*")
+
+def get_valid_password():
+    password = input("What is your password")
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid, Try Again!")
+        password = input("What is your password")
+    return password
+main()

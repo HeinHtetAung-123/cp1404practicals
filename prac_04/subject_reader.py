1689e75c3f5323f001ec385578c1da326448f54c
hein <heinhtetaung1@my.jcu.edu.au>
2025-02-23 21:36:45 +0800

    Made it print the list inside a list essentially a nested loop and as well as the load_data function to return the data. And added a new function to display subject details in the desired format as instructed.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,25 +5,34 @@ Data file -> lists program
 
 FILENAME = "subject.data.txt"
 
-
 def main():
     data = load_data()
-    print(data)
-
+    display_subject_details(data)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+
+        print(line)
+        print(repr(line))
+
+        line = line.strip()
+        parts = line.split(',')
+        print(parts)
+
+        parts[2] = int(parts[2])
+        data.append(parts)
+        print(data)
+
         print("----------")
     input_file.close()
+    return data
 
+def display_subject_details(data):
+    """Displaying the details of the subject"""
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students.")
 
 main()

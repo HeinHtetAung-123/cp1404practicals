794a29efd7abc2e41c28cda128033a58f4816cbc
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-07 21:44:47 +0800

    Copied the code from state_names.py.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,5 +12,5 @@ while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
-        print("Invalid short state")
+        print("Invalid short state!")
     state_code = input("Enter short state: ")

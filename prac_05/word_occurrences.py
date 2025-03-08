b6c56c053460a918a26fe7cc5163c7021e023470
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-08 14:28:43 +0800

    Wrote a program to count the occurrences of words in a string from the user, then print the counts of how many of each word are in it then sorted the output. And finally make it align dynamically so the numbers are in one nice column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,10 +1,10 @@
 word_to_count = {}
 word_count = 0
 word_length = 0
-text  = sorted(input("What is the text:").split())
-print(text)
-for word in text:
-    word_count = text.count(word)
+string_input  = sorted(input("What is the text:").split())
+print(string_input)
+for word in string_input:
+    word_count = string_input.count(word)
     word_to_count[word] = word_count
 print(word_to_count)
 for word in word_to_count:

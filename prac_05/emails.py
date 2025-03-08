965e556d2853305d750f9089e2c847db760a5c8f
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-08 15:42:42 +0800

    Store users' emails and names (values) in a dictionary until they enter a blank one. Then, extract a name from the email and loop through the dictionary and print them out.

---
+++ prac_05/emails.py
@@ -0,0 +1,30 @@
+def main():
+    """ Stores users' emails and names in a dictionary."""
+    SPECIAL_CHAR = "@"
+    email_to_name = {}
+    email = input("Email:")
+    while email != "":
+        if SPECIAL_CHAR not in email:
+            print("The email must contain (@)!")
+            email = input("Email:")
+        name = extract_name(email)
+        question = input(f"Is your name {name}? (Y/N)").strip().upper()
+        if question != "Y" and question != "":
+            name = input("Name:")
+        email_to_name[name] = name
+        email = input("Email:")
+    print_email(email_to_name)
+
+def extract_name(email):
+    """Extracting the name from the email by splitting at '@'."""
+    name_part = email.split("@")[0]
+    name_parts = name_part.split('.')
+    name = " ".join(name_parts).title()
+    return name
+
+def print_email(email_to_name):
+    """Printing email and the name"""
+    for email,name in email_to_name.items():
+        print (f"{name} ({email})")
+
+main()

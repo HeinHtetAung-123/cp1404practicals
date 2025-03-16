1bb3ce728760fdda14d80aa0c638c3985503af57
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-16 15:43:50 +0800

    Wrote a program that allows a user to enter the colour name and show hexadecimal colour codes

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
 COLOR_TO_CODE = {"Absolute Zero": "#0048ba", "Apricot": "#fbceb1", "Amethyst": "#9966cc", "Barn Red": "#7c0a02",
                 "Beige": "#f5f5dc", "Carnelian": "#b31b1b", "Dark Brown": "#654321", "Egyptian Blue" : "#1034a6"}
 print(COLOR_TO_CODE)
-
 for color,code in COLOR_TO_CODE.items():
     print(f"{color:<13} is {code}")
 color_name= input("Enter Color Name: ").title()

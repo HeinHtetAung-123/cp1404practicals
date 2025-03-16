4c15ec75a1bc734cf948d19339d90ce1d3c0b68a
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-16 15:54:37 +0800

    Wrote a program to count the occurrences of words in a string.Then did the sorting alphabetically.And aligned the outputs so the numbers are in one vertical column.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,10 +3,12 @@ word_count = 0
 word_length = 0
 string_input  = sorted(input("What is the text:").split())
 print(string_input)
+
 for word in string_input:
     word_count = string_input.count(word)
     word_to_count[word] = word_count
 print(word_to_count)
+
 for word in word_to_count:
     if len(word) > word_length:
         word_length = len(word)

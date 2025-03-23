0f0aed42a7af08fe6e45c45c5ca2ca11b310bf88
hein <heinhtetaung1@my.jcu.edu.au>
2025-03-23 18:06:25 +0800

    First of all, defined the these methods:__init__,  __str__ and then get_age(),lastly,is_vintage()

---
+++ prac_06/guitar.py
@@ -0,0 +1,20 @@
+CURRENT_YEAR = 2024
+VINTAGE_YEAR = 50
+class Guitar:
+    def __init__(self,name="", year=0,cost=0.0):
+        """Initialize the constructor with the respective names, years, and costs of guitars."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Calling this method to print the result"""
+        return f"{self.name},{self.year}, ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Calculating and returning the age of the guitar"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Returning True or False when the guitar is vintage or not """
+        return self.get_age() >= VINTAGE_YEAR
